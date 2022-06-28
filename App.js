c637ca1bde1d1117e81571809226cae1dc6adc65
Heshan <heshan.imedha@gmail.com>
2022-06-28 13:02:24 +0530

    Global Colors & Styling

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 
 import AddPlace from "./screens/AddPlace";
-import AllPlace from "./screens/AllPlace";
 import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
+import AllPlace from "./screens/AllPlace";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,11 +15,18 @@ export default function App() {
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlace}
             options={({ navigation }) => ({
+              title: "Your Favorite Places",
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
@@ -29,18 +37,15 @@ export default function App() {
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add a new Place",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
