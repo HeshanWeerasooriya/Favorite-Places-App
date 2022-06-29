6c778e65f3817f397f58104050defdbc56907cc8
Heshan <heshan.imedha@gmail.com>
2022-06-29 17:01:35 +0530

    Added GOOGLE_API_KEY

--- util/location.js
+++ util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCTCDNDtYPCpAD0FaKgHgdzCjMN1QUHnt4";  //THIS KEY IS WRONG!
+const GOOGLE_API_KEY = "AIzaSyApnwyJ3WPVjeACiJG5aQblFSsi08YYwAo";  //THIS KEY IS WRONG!
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
